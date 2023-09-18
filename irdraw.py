aac9607669838d2bfb762e240d9661ab0fd20b5c
alexm-nsk <alex.m.work@yandex.ru>
2023-09-18 16:46:34 +0700

    intermediate

--- irdraw.py
+++ irdraw.py
@@ -37,6 +37,19 @@ def get_piped_input():
 
 FUNC_FILL = "#cbcbcb"
 FUNC_STROKE = "#0b0b0b"
+FUNC_MARGIN = 3
+FUNC_ROUND = 3
+
+def draw_node(dwg, area, node):
+    rect = svgwrite.shapes.Rect(
+        insert=(area["left"] + FUNC_MARGIN, area["top"] + FUNC_MARGIN),
+        size=(area["width"]-FUNC_MARGIN*2, area["height"] - FUNC_MARGIN*2),
+        rx=FUNC_ROUND,
+        ry=FUNC_ROUND,
+        fill=FUNC_FILL,
+        stroke=FUNC_STROKE)
+
+    dwg.add(rect)
 
 
 def ir_render_to_svg(functions: list, area: dict) -> str:
@@ -45,11 +58,22 @@ def ir_render_to_svg(functions: list, area: dict) -> str:
     image_height = area["height"]
 
     dwg = svgwrite.Drawing("../test.svg", size=(image_width, image_height))
-    rect = svgwrite.shapes.Rect(
-        insert=(10, 10), size=(790, 590), rx=3, ry=3, fill=FUNC_FILL, stroke=FUNC_STROKE
-    )
-    dwg.add(rect)
 
+    for func in functions:
+        func.num_subs = func.num_subnodes()
+    total_subnodes = sum([func.num_subs for func in functions])
+
+    left = 0
+    for func in functions:
+        function_width = image_width * (func.num_subs / total_subnodes)
+
+        draw_node(dwg,
+                  dict(left=left,
+                       top=0,
+                       width=function_width,
+                       height=image_height),
+                  func)
+        left += function_width
     # dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, "%")))
     # dwg.add(dwg.text("Test", insert=(0, 5), fill="red"))
     dwg.save()
@@ -57,10 +81,9 @@ def ir_render_to_svg(functions: list, area: dict) -> str:
 
 if __name__ == "__main__":
     print("IR Draw utility renders Cloud Sisal IR's presented in JSON")
-    area = dict(width=800, height=600)
+    area = dict(width=1280, height=600)
     with open("ir.json", "r") as file_:
         data = python_names(json.load(file_))
 
     functions = [Node(func) for func in data["functions"]]
     ir_render_to_svg(functions, area)
-    print(functions[0].num_subnodes())
